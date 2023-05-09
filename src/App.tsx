c93e410884fb107ea0b868fc143462d5222711e2
FullStackMystery <my.secret.github@gmail.com>
2023-05-09 17:45:37 -0500

    Use State

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 let a: string = '1';
@@ -8,10 +7,30 @@ interface Props {
   title: string
 }
 
+interface Task {
+  id: number,
+  title: string,
+  description: string,
+  completed: boolean
+}
+
 function App({title}: Props) {
+
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 1,
+      title: "Primero",
+      description: "This is a description",
+      completed: false
+    }
+  ]);
+
   return (
     <div className="App">
       <h1>{title}</h1>
+      {tasks.map(task =>(
+        <div>{task.title}</div>
+      ))}
     </div>
   );
 }
